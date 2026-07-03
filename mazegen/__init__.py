e807ee83f439614a7f66a86002a2bb9a9fb453c1
val <valmoral.42.fr@gmail.com>
2026-07-03 17:41:59 +0200

    clean

--- mazegen/__init__.py
+++ mazegen/__init__.py
@@ -3,4 +3,4 @@ from .maze import Maze
 from .generator import visit, MazeGenerator
 from .solver import solve_maze
 
-__all__ = ["Cell", "wall_map", "Maze", "visit", "solve_maze"]
+__all__ = ["Cell", "wall_map", "Maze", "visit", "solve_maze", "MazeGenerator"]
